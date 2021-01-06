f6dad083482f861eb60701bfb4a5c5a04239265f
Derek Miranda <derek.miranda.1992@gmail.com>
2021-01-05 17:39:04 -0800

    Toggle recording btn

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 function App() {
+  const [recording, updateRecording] = useState(false);
+
+  const toggleRecording = () => {
+    updateRecording(!recording);
+  };
+
   return (
     <div className="app">
       <h1 className="app__header">Acapella Recorder</h1>
       <div className="record">
-        <button className="record__btn">Record</button>
+        <button className="record__btn" onClick={toggleRecording}>
+          {recording ? "Recording..." : "Record"}
+        </button>
         <p className="record__description">Record a first track!</p>
       </div>
     </div>
