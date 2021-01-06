c2ea2f56c2890e1952fdf6268ddc944cea5935a6
Derek Miranda <derek.miranda.1992@gmail.com>
2021-01-05 16:36:13 -0800

    Initial client markup

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__header">Acapella Recorder</h1>
+      <div className="recorder">
+        <button>Record</button>
+        <p className="recorder__description">Record a first track!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
